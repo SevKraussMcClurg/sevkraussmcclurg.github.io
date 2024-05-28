223a4cf3d304d5ed8353eba1921b943a40a94017
SevKraussMcClurg <skraussmcclurg@eastsideprep.org>
2024-05-28 14:10:13 -0700

    light dark demo

---
+++ LightDark/js/script.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector(".btn-toggle");
+const theme = document.querySelector("#theme-link");
+
+btn.addEventListener("click", function () {
+    if (theme.getAttribute("href") == "css/light.css") {
+        theme.href = "css/dark.css";
+    } else {
+        theme.href = "css/light.css";
+    }
+}); 
